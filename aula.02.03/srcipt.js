839621518e9afb35b0b0561e10e31af4b6403989
igorrochavitorino-ui <igorrochavitorino@gmail.com>
2026-03-17 15:35:19 -0300

    Delete aula.02.03 directory

--- aula.02.03/srcipt.js
+++
@@ -1,9 +0,0 @@
-  function exibir(){
-            const nomeDigitado = document. getElementById('nome').value
-            const valorExibido = document.getElementById('resultado')
-            
-            // valorExibido. innerText = ' o nome digitiadao é: $ {nomeDigitado}'
-
-            valorExibido.innerHTML= <h1> (`o nome digitiado é: ${nomeDigitado})</h1>
-
-  }
